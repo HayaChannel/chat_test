f83d87c813dd213dd9a3e6efd448c92f63af747e
HayaChannel <hayato@hayachannel.com>
2016-10-18 01:00:58 -0700

    modify controller as api

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  resources :users
-  resources :messages
-  resources :rooms
-
+  namespace :api, format: 'json' do
+    namespace :v1 do
+      resources :users
+      resources :messages
+      resources :rooms
+    end
+  end
 end
